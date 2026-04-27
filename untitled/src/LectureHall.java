14d7ac9279087646acdb1958648b514e4bb7584c
Alaa0139 <alaa.alsaleh139@gmail.com>
2026-04-27 23:41:42 +0300

    message

--- untitled/src/LectureHall.java
+++ untitled/src/LectureHall.java
@@ -1,3 +1,29 @@
 public class LectureHall extends Venue{
+    private boolean hasSmartBoard;
 
+    public LectureHall(String venueName, int maxCapacity,boolean hasSmartBoard ) {
+        super(venueName, maxCapacity);
+        this.hasSmartBoard=hasSmartBoard;
+    }
+
+    public void sethasSmartBoard(boolean hasSmartBoard) {
+        this.hasSmartBoard = hasSmartBoard;
+    }
+
+    public boolean gethasSmartBoarde(){
+        return this.hasSmartBoard;
+    }
+
+    @Override
+    public String getVenueType(){
+        return "LectureHall" ;
+    }
+
+    @Override
+    public void printDetails(){
+        System.out.println("Venue name: " + getVenueName());
+        System.out.println("Venue type: " + getVenueType());
+        System.out.println("Max capacity: " + getMaxCapacity());
+        System.out.println("hasSmartBoard: "+gethasSmartBoarde());
+    }
 }
--- untitled/src/Venue.java
+++ untitled/src/Venue.java
@@ -2,6 +2,27 @@ public abstract class Venue {
     private String venueName;
     private int maxCapacity;
 
+    public Venue(String venueName, int maxCapacity) {
+        this.venueName = venueName;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
     // Abstract methods
     public abstract void printDetails();
     public abstract String getVenueType();

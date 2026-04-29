1f204b73b06d7b4c0f5d1c3c16191ccf8e3acb70
Alaa0139 <alaa.alsaleh139@gmail.com>
2026-04-29 23:32:14 +0300

    message

--- src/VenueManager.java
+++ src/VenueManager.java
@@ -3,8 +3,22 @@ import java.util.ArrayList;
 public class VenueManager {
     private ArrayList<Venue> venues = new ArrayList<>();
 
+    public boolean venueExists(String name) {
+        for (Venue venue : venues) {
+            if (venue.getVenueName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void addVenue(Venue venue) {
-        venues.add(venue);
+        if (venueExists(venue.getVenueName())==false) {
+            venues.add(venue);
+        }
+        else {
+            System.out.println("venue already exist");
+        }
     }
 
     public void viewAllVenues() {
@@ -18,7 +32,49 @@ public class VenueManager {
         }
     }
 
+    public Venue searchVenue(String name){
+
+        for (Venue venue : venues) {
+            if (venue.getVenueName().equals(name) ){
+                return venue;
+            }
+        }
+
+            return null;
+        }
+
+        public void showVenuDetails(String name){
+        Venue venue=searchVenue(name);
+        if (venue!=null){
+            venue.printDetails();
+        }
+        else {
+            System.out.println("venue not found");
+        }
+
+        }
+
+        public boolean checkAttendanceValidation(int attendance,Venue venue){
+        if (attendance<=venue.getMaxCapacity()) {
+            if (searchVenue(venue.getVenueName()) != null) {
+                System.out.println("valid attendance");
+                return true;
+            } else {
+                System.out.println("Invalid attendance");
+                return false;
+            }
+        }
+        else {
+                System.out.println("venue not found" );
+                return false;
+            }
+        }
+
+
+    }
 
 
 
-}
+
+
+

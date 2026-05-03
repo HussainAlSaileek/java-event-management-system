0c1916e7918ea0e54e8d3fcb0946bbfb127c04dc
Alaa0139 <alaa.alsaleh139@gmail.com>
2026-05-03 13:54:22 +0300

    message

--- src/VenueMenu.java
+++ src/VenueMenu.java
@@ -5,8 +5,8 @@ public class VenueMenu {
     public void setVenueList(VenueManager venueList) {
         Venue v1=new SportsArea("feild 1",85,"Football");
         Venue v2=new LectureHall("room:1001",120,true);
-        Venue v3=new ConferenceHall("feild 1",85,true);
-        Venue v4=new PublicSpace("feild 1",85);
+        Venue v3=new ConferenceHall("Hall A",90,true);
+        Venue v4=new PublicSpace("feild 1",450);
         venueList.addVenue(v1);
         venueList.addVenue(v2);
         venueList.addVenue(v3);
